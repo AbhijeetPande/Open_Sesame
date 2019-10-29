7d7e4aa469439cba5e0020159c4dffdddc9e1d08
Abhijeet Pande <abhijeet.cbse@gmail.com>
2019-10-29 10:00:53 -0400

    Introduced User choice, Invalid input detection
    Now the User can choose whether or not they want to launch all the applications. I also introduced invalid input recognition, which does not trigger due to uppercase / lowercase differences.

--- Open_Sesame.cmd
+++ Open_Sesame.cmd
@@ -7,23 +7,58 @@ REM This script is meant to launch my working environment everyday when I show u
 @echo off
 
 title This is your first batch script!
-echo Welcome to batch scripting!
+echo Good Morning!
+echo The Usual for you, sir?
+:loop
+set /p primary="Enter Selection(Y/N): "
+
+REM If the user decides that he wants to activate all applications:
+IF /I %primary% EQU Y ( SET VSCode=Y & SET Outlook=Y & SET TestComplete=Y & SET Excel=Y & SET Google=Y & pause )
+
+REM If the user wants to activate certain applications only:
+
+IF /I %primary% NEQ Y IF /I %primary% EQU N ( 
+    echo So what would you prefer instead of the usual, sir? 
+    set /p VSCode="Enter Selection for VSCode (Y/N): " 
+    set /p Outlook="Enter Selection for Outlook (Y/N): " 
+    set /p TestComplete="Enter Selection for TestComplete (Y/N): " 
+    set /p Excel="Enter Selection for Excel (Y/N): " 
+    set /p Google="Enter Selectionfor Google (Y/N): " 
+    pause )
+
+REM If the user made an invalid entry (Not Y or N):
+IF /I %primary% NEQ Y IF /I %primary% NEQ N (
+    echo This is an invalid entry. try again please.
+    GOTO loop
+)
+
+REM If the user says no to everything:
+
 pause
+
+
+REM Opening VSCode:
+ cd "C:\Users\pandea\AppData\Local\Programs\Microsoft VS Code"
+ start Code.exe
+
+REM Opening TestComplete:
+REM cd "C:\Program Files (x86)\SmartBear\TestComplete 14\x64\Bin"
+REM start TestComplete.exe
+
+REM Opening Outlook and Excel:
+ cd "C:\Program Files (x86)\Microsoft Office\root\Office16"
+ start OUTLOOK.exe
+ start EXCEL.exe
 
 REM Opening google:
 
-start "" https://penweb-uat-web.penweb.local/member/auth/app/login
-start "" https://penweb-pre-web.penweb.local/member/auth/app/login
+ start "" https://penweb-uat-web.penweb.local/member/auth/app/login
+ start "" https://penweb-pre-web.penweb.local/member/auth/app/login
 
-REM Opening VSCode:
-cd "C:\Users\pandea\AppData\Local\Programs\Microsoft VS Code"
-start Code.exe
 
-SET foo=bar
-REM Note: Do not use whitespace between the names and value, SET foo = bar will not work.
 
-SET /A four=2+2
-REM Note: The /A allows for arithmetic operation when setting a variable.
+
+
 
 REM By default, variables are global to your entire command  prompt session. Call the SETLOCAL command to make variables local to the scope of your script.
-REM C:\Users\pandea\Open_Sesame.cmd
+REM C:\Users\pandea\Desktop\Open_Sesame.cmd
